1170fbd4b3e919a789efb370895cc08819829bcb
PIYUSH1525 <negipiyush125@gmail.com>
2023-03-26 00:06:36 +0530

    added css and java script

--- calculator/main.js
+++ calculator/main.js
@@ -20,9 +20,27 @@ for (let key of keys){
 
             display_output.innerHTML =result;
         } else if (value == "brackets"){
-            if (input.indexOf("(") == -1 || input.indexOf("(") != -1 && input.indexOf(")") != -1 && input.lastIndexOf("(") < input.lastIndexOf(")")){
+            if (
+                input.indexOf("(") == -1 ||
+                input.indexOf("(") != -1 &&
+                input.indexOf(")") != -1 &&
+                input.lastIndexOf("(") < input.lastIndexOf(")")
+                ){
                 input +="(";
+            }else if (
+                input.indexOf("(") != -1 &&
+                 input.indexOf(")") ==-1||
+                 input.indexOf("(") != -1 &&
+                 input.indexOf(")") != -1 &&
+                 input.indexOf("(") > input.lastIndexOf(")")
+                 ) {
+                input -=")";
             }
+
+            display_input.innerHTML = input;
+        } else{
+            input+= value;
+            display_input.innerHTML= input;
         }
     })
 }
